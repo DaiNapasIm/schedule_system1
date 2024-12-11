37cd9dd6e75fb7e1efcf2e739c053ebf1c220c20
DaiNapasIm <goodboy1221k@gmail.com>
2024-12-11 19:41:16 +0300

    Первый коммит: добавление проекта

--- schedule_system/schedule_system/settings.py
+++ schedule_system/schedule_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3@#o-k8(62(r-b0y21@3k!j4=f^6d3ne=*91rm^d-uqk($&b$a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['THEFIDELISCREATORS.pythonanywhere.com']
 
 
 # Application definition
